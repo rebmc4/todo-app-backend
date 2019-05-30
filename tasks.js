f2404dc5688d8d8eb9918db992bcef32ed346762
Rachel <rebmc4@gmail.com>
2019-05-30 20:54:24 +0100

    hook up backend to database code

--- tasks.js
+++ tasks.js
@@ -2,20 +2,36 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql');
 app.use(cors());
 
-app.get('/tasks', function (request, response) {
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "Tasks"
+});
 
+app.get("/tasks", function(request, response) {
   const username = request.query.username;
-
-  const someJson = {
-    tasks: [
-      {task: "buy some milk", completed: false, id: 1},
-      {task: "walk the dog", completed: true, id: 2},
-      {task: "go for a walk", completed: false, id: 3}
-    ]
-  };
-  response.json(someJson);
-})
+  let queryToExecute = "SELECT * FROM Tasks";
+  // if (username) {
+  //   query =
+  //     "SELECT * FROM Task JOIN User on Task.UserId = User.UserId WHERE User.Username = " +
+  //     connection.escape(username);
+  // }
+  connection.query(queryToExecute, (err, queryResults) => {
+    if (err) {
+      console.log("Error fetching tasks", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.json({
+        tasks: queryResults
+      });
+    }
+  });
+});
 
 module.exports.handler = serverless(app);
